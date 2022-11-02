3b45ab6a460a7b4137a507778f943c8fc5eb1857
OUMAR CAMARA <mamealphasy23@gmail.com>
2022-11-02 13:23:30 +0000

    alpha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InscriptionElectionComponent } from './inscription-election/inscription-election.component';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 
 
 const routes: Routes = [
-  { path: 'inscription-electeur', component: InscriptionElectionComponent }
+  { path: 'inscription-electeur', component: InscriptionElectionComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'connexion', component: ConnexionComponent }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { InscriptionElectionComponent } from './inscription-election/inscription-election.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { ConnexionComponent } from './connexion/connexion.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InscriptionElectionComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent,
+    ConnexionComponent
   ],
   imports: [
     BrowserModule,
